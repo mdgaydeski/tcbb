834cea6b4f8f51de1a6e0e6a5897b29645efa7c6
mdgaydeski <mdgaydeski@gmail.com>
2021-03-04 18:33:23 -0600

    Add navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+import './Navigation.css';
 
-const Navigation = () => {
-    return (
-        <p>Navbar goes here</p>
-    );
-}
+const Navigation = () => (
+    <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+        <div className='container-fluid'>
+            <NavLink className='navbar-brand' to='/'>Twin Cities Brass Band</NavLink>
+            <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarCollapse' aria-controls='navbarCollapse' aria-expanded='false' aria-label='Toggle navigation'>
+                <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse' id='navbarCollapse'>
+                <ul className='navbar-nav w-100 justify-content-around'>
+                    <li className='nav-item'>
+                        <NavLink className='nav-link' to='/' exact>Home</NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink className='nav-link' to='/about' exact>About</NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink className='nav-link' to='/events'>Events</NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink className='nav-link' to='/media'>Media</NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink className='nav-link' to='/contact'>Contact</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+);
 
 export default Navigation;
