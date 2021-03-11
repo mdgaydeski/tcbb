1fe8feb14307993d0fb7840d134fd944507515ef
mdgaydeski <mdgaydeski@gmail.com>
2021-03-11 11:48:24 -0600

    Add social media links

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import icon_facebook from '../../resources/icons/icon_facebook.png';
+import icon_instagram from '../../resources/icons/icon_instagram.png';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <footer className='text-light bg-dark position-absolute bottom-0 w-100 text-center'>
-            &copy; {new Date().getFullYear()}
+        <footer className='text-light bg-dark position-absolute bottom-0 w-100 d-flex justify-content-between'>
+            <div />
+            <div>
+                &copy; {new Date().getFullYear()} Twin Cities Brass Band
+            </div>
+            <div>
+                <a href='https://www.facebook.com/twincitiesbrassband/' target='_blank' rel='noreferrer'>
+                    <img src={icon_facebook} alt='Facebook icon' className='icon' />
+                </a>
+                <a href='https://www.instagram.com/twincitiesbrassband/' target='_blank' rel='noreferrer'>
+                    <img src={icon_instagram} alt='Instagram icon' className='icon' />
+                </a>
+            </div>
         </footer>
     );
 }
